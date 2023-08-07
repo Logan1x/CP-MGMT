efa6461b4fca3de5b706627df0f4f5cc55bf4f7b
logan1x <sharmakhushal78@gmail.com>
2023-08-07 17:01:14 +0530

    ad: head tag

--- client/src/app/providers.js
+++ client/src/app/providers.js
@@ -3,6 +3,7 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Head from "next/head";
 
 const client = new ApolloClient({
   uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
@@ -11,10 +12,16 @@ const client = new ApolloClient({
 
 export function Providers({ children }) {
   return (
-    <ApolloProvider client={client}>
-      <CacheProvider>
-        <ChakraProvider>{children}</ChakraProvider>
-      </CacheProvider>
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>MGMT</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <ApolloProvider client={client}>
+        <CacheProvider>
+          <ChakraProvider>{children}</ChakraProvider>
+        </CacheProvider>
+      </ApolloProvider>
+    </>
   );
 }
